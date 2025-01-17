e33496675242d234ca75fe68ae6ef6fc3c023b04
Leaundrae Mckinney <pytechdrae@gmail.com>
2025-01-17 09:20:54 -0500

    Building Avatar Registry..again

---
+++ client/src/utils/helpers/avatarRegistry.tsx
@@ -0,0 +1,65 @@
+import aquaFlowUtilities from '@/assets/images/avatars/aqua-flow-utilities.jpg';
+import buzzMarketingGroup from '@/assets/images/avatars/buzz-marketing-group.jpg';
+import byteWise from '@/assets/images/avatars/byte-wise.jpg';
+import danielCarter from '@/assets/images/avatars/daniel-carter.jpg';
+import ecofuelEnergy from '@/assets/images/avatars/ecofuel-energy.jpg';
+import elevateEducation from '@/assets/images/avatars/elevate-education.jpg';
+import ellaPhillips from '@/assets/images/avatars/ella-phillips.jpg';
+import emmaRichardson from '@/assets/images/avatars/emma-richardson.jpg';
+import ethanClark from '@/assets/images/avatars/ethan-clark.jpg';
+import flavorFiesta from '@/assets/images/avatars/flavor-fiesta.jpg';
+import greenPlate from '@/assets/images/avatars/green-plate.jpg';
+import harperEdwards from '@/assets/images/avatars/harper-edwards.jpg';
+import jamesThompson from '@/assets/images/avatars/james-thompson.jpg';
+import liamHughes from '@/assets/images/avatars/liam-hughes.jpg';
+import lilyRamirez from '@/assets/images/avatars/lily-ramirez.jpg';
+import masonMartines from '@/assets/images/avatars/mason-martines.jpg';
+import nimbusDataStorage from '@/assets/images/avatars/nimbus-data-storage.jpg';
+import pixelPlayground from '@/assets/images/avatars/pixel-playground.jpg';
+import rinaSato from '@/assets/images/avatars/rina-sato.jpg';
+import savoryBytes from '@/assets/images/avatars/savory-bytes.jpg';
+import sebastianCook from '@/assets/images/avatars/sebastian-cook.jpg';
+import serenitySpaAndWellness from '@/assets/images/avatars/serenity-spa-and-wellness.jpg';
+import sofiaPeterson from '@/assets/images/avatars/sofia-peterson.jpg';
+import sparkElectricSolutions from '@/assets/images/avatars/spark-electric-solutions.jpg';
+import sunPark from '@/assets/images/avatars/sun-park.jpg';
+import swiftRideShare from '@/assets/images/avatars/swift-ride-share.jpg';
+import technovaInnovations from '@/assets/images/avatars/technova-innovations.jpg';
+import urbanServicesHub from '@/assets/images/avatars/urban-services-hub.jpg';
+import williamHarris from '@/assets/images/avatars/william-harris.jpg';
+import yunaKim from '@/assets/images/avatars/yuna-kim.jpg';
+
+export const AvatarRegistry = {
+    "aqua-flow-utilities": aquaFlowUtilities,
+    "buzz-marketing-group": buzzMarketingGroup,
+    "byte-wise": byteWise,
+    "daniel-carter": danielCarter,
+    "ecofuel-energy": ecofuelEnergy,
+    "elevate-education": elevateEducation,
+    "ella-phillips": ellaPhillips,
+    "emma-richardson": emmaRichardson,
+    "ethan-clark": ethanClark,
+    "flavor-fiesta": flavorFiesta,
+    "green-plate": greenPlate,
+    "harper-edwards": harperEdwards,
+    "james-thompson": jamesThompson,
+    "liam-hughes": liamHughes,
+    "lily-ramirez": lilyRamirez,
+    "mason-martines": masonMartines,
+    "nimbus-data-storage": nimbusDataStorage,
+    "pixel-playground": pixelPlayground,
+    "rina-sato": rinaSato,
+    "savory-bytes": savoryBytes,
+    "sebastian-cook": sebastianCook,
+    "serenity-spa-and-wellness": serenitySpaAndWellness,
+    "sofia-peterson": sofiaPeterson,
+    "spark-electric-solutions": sparkElectricSolutions,
+    "sun-park": sunPark,
+    "swift-ride-share": swiftRideShare,
+    "technova-innovations": technovaInnovations,
+    "urban-services-hub": urbanServicesHub,
+    "william-harris": williamHarris,
+    "yuna-kim": yunaKim    
+}
+
+export type AvatarKey = keyof typeof AvatarRegistry;
